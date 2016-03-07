1fec054c763ffc3897f162fa134379b85e8bba40
Jay Gheewala <jgheewala@snaproute.com>
2016-03-07 11:07:35 -0800

    skeletion for acl ip tables

--- sysd/rpc/sysd.thrift
+++ sysd/rpc/sysd.thrift
@@ -1,6 +1,15 @@
 namespace go sysd
 typedef i32 int
 typedef i16 uint16
+struct IpTableAclConfig {
+	1 : string Protocol
+	2 : string Name
+	3 : string IpAddr
+	4 : string Access
+	5 : string NetMask
+	6 : i32 IfIndex
+	7 : string Port
+}
 struct ComponentLoggingConfig {
 	1 : string Module
 	2 : string Level
@@ -10,6 +19,10 @@ struct SystemLoggingConfig {
 	2 : string SystemLogging
 }
 service SYSDServices {
+	bool CreateIpTableAclConfig(1: IpTableAclConfig config);
+	bool UpdateIpTableAclConfig(1: IpTableAclConfig origconfig, 2: IpTableAclConfig newconfig, 3: list<bool> attrset);
+	bool DeleteIpTableAclConfig(1: IpTableAclConfig config);
+
 	bool CreateComponentLoggingConfig(1: ComponentLoggingConfig config);
 	bool UpdateComponentLoggingConfig(1: ComponentLoggingConfig origconfig, 2: ComponentLoggingConfig newconfig, 3: list<bool> attrset);
 	bool DeleteComponentLoggingConfig(1: ComponentLoggingConfig config);
